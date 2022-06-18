bedb466cd971115f649355e00ec6d26d7e03f8ce
K-MHOON <azmria117@gmail.com>
2022-06-18 10:16:25 +0900

    feat : Web Ignore 설정 및 application.yml jpa 설정 추가

--- src/main/java/com/example/springsecuritysimpleproject/security/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritysimpleproject/security/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.springsecuritysimpleproject.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -17,14 +19,24 @@ import java.security.cert.Extension;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * 정적 파일에 대한 보안 필터를 해제한다.
+     * 보안 필터를 아예 거치지 않는다. (permitAll과의 차이)
+     * @return
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/user").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
-                .antMatchers("/messages").hasRole("MANAGER")
-                .antMatchers("/config").hasRole("ADMIN")
+                .antMatchers("/messages").hasAnyRole("MANAGER", "USER")
+                .antMatchers("/config").hasAnyRole("ADMIN","USER","MANAGER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
@@ -32,6 +44,10 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /**
+     * 인 메모리 계정을 생성한다.
+     * @return
+     */
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
 
