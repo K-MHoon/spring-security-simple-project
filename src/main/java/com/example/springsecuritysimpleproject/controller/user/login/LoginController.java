2dae5171af72af63d3ee4f3ac130f0ffc84b4e50
K-MHOON <azmria117@gmail.com>
2022-06-19 15:11:52 +0900

    feat : 로그아웃 컨트롤러 추가 및 SecurityContextLogoutHandler 활용하여 로그아웃 적용

--- src/main/java/com/example/springsecuritysimpleproject/controller/user/login/LoginController.java
+++ src/main/java/com/example/springsecuritysimpleproject/controller/user/login/LoginController.java
@@ -1,7 +1,14 @@
 package com.example.springsecuritysimpleproject.controller.user.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Controller
 public class LoginController {
@@ -9,5 +16,15 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(Objects.nonNull(authentication)) {
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        return "redirect:/login";
     }
 }
