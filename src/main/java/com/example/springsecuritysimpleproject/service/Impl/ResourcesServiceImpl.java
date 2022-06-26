a47b94baadfec01b94ca8ce5a336183cf5951ca7
K-MHOON <azmria117@gmail.com>
2022-06-27 08:43:12 +0900

    feat : RoleService, ResourcesService 구현

--- src/main/java/com/example/springsecuritysimpleproject/service/Impl/ResourcesServiceImpl.java
+++ src/main/java/com/example/springsecuritysimpleproject/service/Impl/ResourcesServiceImpl.java
@@ -1,28 +1,41 @@
 package com.example.springsecuritysimpleproject.service.Impl;
 
 import com.example.springsecuritysimpleproject.domain.resources.Resources;
+import com.example.springsecuritysimpleproject.repository.resources.ResourcesRepository;
 import com.example.springsecuritysimpleproject.service.user.ResourcesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
 public class ResourcesServiceImpl implements ResourcesService {
+
+    private final ResourcesRepository resourcesRepository;
+
     @Override
     public Resources getResources(Long id) {
-        return null;
+        return resourcesRepository.findById(id).orElse(new Resources());
     }
 
     @Override
     public List<Resources> getResources() {
-        return null;
+        return resourcesRepository.findAll(Sort.by(Sort.Order.asc("orderNum")));
     }
 
     @Override
     public void createResources(Resources resources) {
-
+        resourcesRepository.save(resources);
     }
 
     @Override
     public void deleteResources(Long id) {
-
+        resourcesRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/springsecuritysimpleproject/service/Impl/RoleServiceImpl.java
+++ src/main/java/com/example/springsecuritysimpleproject/service/Impl/RoleServiceImpl.java
@@ -1,29 +1,40 @@
 package com.example.springsecuritysimpleproject.service.Impl;
 
 import com.example.springsecuritysimpleproject.domain.role.Role;
+import com.example.springsecuritysimpleproject.repository.role.RoleRepository;
 import com.example.springsecuritysimpleproject.service.user.RoleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
 
     @Override
     public Role getRole(Long id) {
-        return null;
+        return roleRepository.findById(id).orElse(new Role());
     }
 
     @Override
     public List<Role> getRoles() {
-        return null;
+        return roleRepository.findAll();
     }
 
     @Override
     public void createRole(Role role) {
-
+        roleRepository.save(role);
     }
 
     @Override
     public void deleteRole(Long id) {
-
+        roleRepository.deleteById(id);
     }
 }
