270062723d14912bc01c89f32bc66f5b5267b572
K-MHOON <azmria117@gmail.com>
2022-07-06 21:26:24 +0900

    feat : Map 기반 MethodSecurityMetadataSource 구현

--- src/main/java/com/example/springsecuritysimpleproject/security/config/MethodSecurityConfig.java
+++ src/main/java/com/example/springsecuritysimpleproject/security/config/MethodSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.springsecuritysimpleproject.security.config;
 
+import com.example.springsecuritysimpleproject.security.factory.MethodResourcesFactoryBean;
+import com.example.springsecuritysimpleproject.service.resource.SecurityResourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.method.MapBasedMethodSecurityMetadataSource;
 import org.springframework.security.access.method.MethodSecurityMetadataSource;
@@ -8,9 +12,25 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 
 @Configuration
 @EnableGlobalMethodSecurity
+@RequiredArgsConstructor
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+    private final SecurityResourceService securityResourceService;
+
     @Override
     protected MethodSecurityMetadataSource customMethodSecurityMetadataSource() {
         return new MapBasedMethodSecurityMetadataSource();
+    }
+
+    @Bean
+    public MapBasedMethodSecurityMetadataSource mapBasedMethodSecurityMetadataSource() {
+        return new MapBasedMethodSecurityMetadataSource(methodResourcesMapFactoryBean().getObject());
+    }
+
+    @Bean
+    public MethodResourcesFactoryBean methodResourcesMapFactoryBean() {
+        MethodResourcesFactoryBean methodResourcesFactoryBean = new MethodResourcesFactoryBean();
+        methodResourcesFactoryBean.setSecurityResourceService(securityResourceService);
+        return methodResourcesFactoryBean;
     }
 }
---
+++ src/main/java/com/example/springsecuritysimpleproject/security/factory/MethodResourcesFactoryBean.java
@@ -0,0 +1,45 @@
+package com.example.springsecuritysimpleproject.security.factory;
+
+import com.example.springsecuritysimpleproject.service.resource.SecurityResourceService;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class MethodResourcesFactoryBean implements FactoryBean<LinkedHashMap<String, List<ConfigAttribute>>> {
+
+    private SecurityResourceService securityResourceService;
+    private LinkedHashMap<String, List<ConfigAttribute>> resourceMap;
+
+
+    public void setSecurityResourceService(SecurityResourceService securityResourceService) {
+        this.securityResourceService = securityResourceService;
+    }
+
+    @Override
+    public LinkedHashMap<String, List<ConfigAttribute>> getObject() {
+
+        if(Objects.isNull(resourceMap)) {
+            init();
+        }
+
+        return resourceMap;
+    }
+
+    private void init() {
+        resourceMap = securityResourceService.getMethodResourceList();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return LinkedHashMap.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
